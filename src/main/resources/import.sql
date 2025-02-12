f87cc62d50937182601faa0c5e0908ec73e12019
ivanGianandrea <ivangianandrea@hotmail.it>
2025-02-12 14:21:31 +0100

    add import

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT into area_pinacoteca(curatore_id,id,nome) values (null,nextval(area_pinacoteca_seq),'area1');
+INSERT into area_pinacoteca(curatore_id,id,nome) values (null,nextval(area_pinacoteca_seq),'area2');
+INSERT into area_pinacoteca(curatore_id,id,nome) values (null,nextval('area_pinacoteca_seq'),'area3');
